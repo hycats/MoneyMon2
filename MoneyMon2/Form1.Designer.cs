a6e9762c4c6a9819144988446dc0b25aea9be4ce
Atsushi Hayashi <hycats@gmail.com>
2017-12-31 02:42:23 +0900

    design 1

--- MoneyMon2/Form1.Designer.cs
+++ MoneyMon2/Form1.Designer.cs
@@ -30,10 +30,27 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cbAccounts = new System.Windows.Forms.ComboBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.cbExpense = new System.Windows.Forms.ComboBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lbExpense = new System.Windows.Forms.Label();
+            this.cbBreakdown = new System.Windows.Forms.ComboBox();
+            this.lbBreakdown = new System.Windows.Forms.Label();
+            this.cbProduct = new System.Windows.Forms.ComboBox();
+            this.lbProduct = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbMoney = new System.Windows.Forms.Label();
+            this.btMoney = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -51,6 +68,8 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Controls.Add(this.panel2);
             this.tabPage1.Location = new System.Drawing.Point(4, 34);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -59,12 +78,64 @@
             this.tabPage1.Text = "TOP";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cbAccounts);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1456, 74);
+            this.panel1.TabIndex = 2;
+            // 
+            // cbAccounts
+            // 
+            this.cbAccounts.FormattingEnabled = true;
+            this.cbAccounts.Location = new System.Drawing.Point(29, 14);
+            this.cbAccounts.Name = "cbAccounts";
+            this.cbAccounts.Size = new System.Drawing.Size(364, 32);
+            this.cbAccounts.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.btMoney);
+            this.panel2.Controls.Add(this.lbMoney);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.lbProduct);
+            this.panel2.Controls.Add(this.cbProduct);
+            this.panel2.Controls.Add(this.lbBreakdown);
+            this.panel2.Controls.Add(this.cbBreakdown);
+            this.panel2.Controls.Add(this.lbExpense);
+            this.panel2.Controls.Add(this.cbExpense);
+            this.panel2.Controls.Add(this.monthCalendar1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(1459, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(504, 1187);
+            this.panel2.TabIndex = 1;
+            // 
+            // cbExpense
+            // 
+            this.cbExpense.FormattingEnabled = true;
+            this.cbExpense.Location = new System.Drawing.Point(127, 406);
+            this.cbExpense.Name = "cbExpense";
+            this.cbExpense.Size = new System.Drawing.Size(352, 32);
+            this.cbExpense.TabIndex = 1;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.FirstDayOfWeek = System.Windows.Forms.Day.Monday;
+            this.monthCalendar1.Location = new System.Drawing.Point(9, 12);
+            this.monthCalendar1.MaxSelectionCount = 1;
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            // 
             // tabPage2
             // 
             this.tabPage2.Location = new System.Drawing.Point(4, 34);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1966, 1147);
+            this.tabPage2.Size = new System.Drawing.Size(1966, 1193);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "家計簿";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -74,7 +145,7 @@
             this.tabPage3.Location = new System.Drawing.Point(4, 34);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(1966, 1147);
+            this.tabPage3.Size = new System.Drawing.Size(1966, 1193);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "グラフ";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -84,21 +155,107 @@
             this.tabPage4.Location = new System.Drawing.Point(4, 34);
             this.tabPage4.Name = "tabPage4";
             this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage4.Size = new System.Drawing.Size(1966, 1147);
+            this.tabPage4.Size = new System.Drawing.Size(1966, 1193);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "設定";
             this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 1209);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1974, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lbExpense
+            // 
+            this.lbExpense.AutoSize = true;
+            this.lbExpense.Location = new System.Drawing.Point(46, 409);
+            this.lbExpense.Name = "lbExpense";
+            this.lbExpense.Size = new System.Drawing.Size(75, 25);
+            this.lbExpense.TabIndex = 2;
+            this.lbExpense.Text = "費目：";
+            // 
+            // cbBreakdown
+            // 
+            this.cbBreakdown.FormattingEnabled = true;
+            this.cbBreakdown.Location = new System.Drawing.Point(127, 444);
+            this.cbBreakdown.Name = "cbBreakdown";
+            this.cbBreakdown.Size = new System.Drawing.Size(352, 32);
+            this.cbBreakdown.TabIndex = 3;
+            // 
+            // lbBreakdown
+            // 
+            this.lbBreakdown.AutoSize = true;
+            this.lbBreakdown.Location = new System.Drawing.Point(46, 447);
+            this.lbBreakdown.Name = "lbBreakdown";
+            this.lbBreakdown.Size = new System.Drawing.Size(75, 25);
+            this.lbBreakdown.TabIndex = 4;
+            this.lbBreakdown.Text = "内訳：";
+            // 
+            // cbProduct
+            // 
+            this.cbProduct.FormattingEnabled = true;
+            this.cbProduct.Location = new System.Drawing.Point(127, 482);
+            this.cbProduct.Name = "cbProduct";
+            this.cbProduct.Size = new System.Drawing.Size(352, 32);
+            this.cbProduct.TabIndex = 5;
+            // 
+            // lbProduct
+            // 
+            this.lbProduct.AutoSize = true;
+            this.lbProduct.Location = new System.Drawing.Point(46, 485);
+            this.lbProduct.Name = "lbProduct";
+            this.lbProduct.Size = new System.Drawing.Size(75, 25);
+            this.lbProduct.TabIndex = 6;
+            this.lbProduct.Text = "品名：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(127, 1054);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(352, 31);
+            this.textBox1.TabIndex = 7;
+            // 
+            // lbMoney
+            // 
+            this.lbMoney.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lbMoney.AutoSize = true;
+            this.lbMoney.Location = new System.Drawing.Point(46, 1057);
+            this.lbMoney.Name = "lbMoney";
+            this.lbMoney.Size = new System.Drawing.Size(75, 25);
+            this.lbMoney.TabIndex = 8;
+            this.lbMoney.Text = "金額：";
+            // 
+            // btMoney
+            // 
+            this.btMoney.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btMoney.Location = new System.Drawing.Point(272, 1091);
+            this.btMoney.Name = "btMoney";
+            this.btMoney.Size = new System.Drawing.Size(207, 43);
+            this.btMoney.TabIndex = 9;
+            this.btMoney.Text = "入力";
+            this.btMoney.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1974, 1231);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -109,6 +266,20 @@
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox cbAccounts;
+        private System.Windows.Forms.ComboBox cbExpense;
+        private System.Windows.Forms.Label lbExpense;
+        private System.Windows.Forms.Label lbBreakdown;
+        private System.Windows.Forms.ComboBox cbBreakdown;
+        private System.Windows.Forms.Label lbProduct;
+        private System.Windows.Forms.ComboBox cbProduct;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbMoney;
+        private System.Windows.Forms.Button btMoney;
     }
 }
 
